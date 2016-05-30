7b45f695fddb0247cf7b2f3d4b08b6ef5e7fdace
jasmany lara <jasmanylara@hotmail.com>
2016-05-30 13:59:31 -0400

    Cambiando host

--- app/config/database.php
+++ app/config/database.php
@@ -54,8 +54,7 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => '127.7.8.130',
-			'port'      => '3306',
+			'host'      => '127.7.8.130:3306',
 			'database'  => 'aymarapp',
 			'username'  => 'adminFJgafcN',
 			'password'  => 'SdBEiuTT_6d3',
