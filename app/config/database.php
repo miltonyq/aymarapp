88213959c2baa823f86fbbe44017c32d1602dc28
jasmany lara <jasmanylara@hotmail.com>
2016-06-01 15:52:06 -0400

    database

--- app/config/database.php
+++ app/config/database.php
@@ -54,7 +54,7 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => '127.0.0.1',
+			'host'      => 'https://aymarapp-pamh.rhcloud.com/phpmyadmin',
 			'database'  => 'aymarapp',
 			'username'  => 'adminFJgafcN',
 			'password'  => 'SdBEiuTT_6d3',
